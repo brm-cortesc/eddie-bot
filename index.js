59aa018cc158be8c3110ee9cf1291450032e3a6a
Cristian Cortés <cristian.cortes@brm.com.co>
2018-04-20 15:09:34 -0500

    display array

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ const connection = mysql.createConnection({
 	user: process.env.USER,
 	password: process.env.PASS, 
 	database: process.env.DB 
-})
+});
+
+const { RTMClient, WebClient } = require('@slack/client');
+
+const token = process.env.TOKEN; 
+
+// The client is initialized and then started to get an active connection to the platform
+const rtm = new RTMClient(token);
 
 
 //:::::::::MYSQl::::::::://
@@ -35,22 +42,12 @@ const connection = mysql.createConnection({
 
 // });
 
-app.listen(3000)
+// app.listen(3000)
 
 
 
 
 //:::::::::SLACK CLIENT::::::::://
-
-const { RTMClient, WebClient } = require('@slack/client');
-
-
-// An access token (from your Slack app or custom integration - usually xoxb)
-const token = process.env.TOKEN; 
-
-// The client is initialized and then started to get an active connection to the platform
-const rtm = new RTMClient(token);
-// rtm.start();
 
 // This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
 // See the "Combining with the WebClient" topic below for an example of how to get this ID
@@ -60,18 +57,22 @@ rtm.start();
 // Need a web client to find a channel where the app can post a message
 const web = new WebClient(token);
 
+const arr = [];
+
 web.channels.list()
   .then((res) => {
   	// console.log('entre')
     // Take any channel for which the bot is a member
     const channel = res.channels.find(c => c.is_member);
 
-    console.log(res)
+    // console.log(res)
+
+    arr.push(res)
 
     if (channel) {
       // We now have a channel ID to post a message in!
       // use the `sendMessage()` method to send a simple string to a channel using the channel ID
-      rtm.sendMessage('Hello, world!', channel.id)
+      rtm.sendMessage('Holandas', channel.id)
         // Returns a promise that resolves when the message is sent
         .then((msg) => console.log(`Message sent to channel ${channel.name} with ts:${msg.ts}`))
         .catch(console.error);
@@ -79,4 +80,15 @@ web.channels.list()
       console.log('This bot does not belong to any channel, invite it to at least one and try again');
     }
 });
+
+app.get('/', (req, res, next)=>{
+
+	res.send(arr);
+
+});
+
+app.listen(3000)
+
+console.log('servidor en http://localhost:'+ 3000)
+
 
